5175aea51eb7bd0b9ae5a3a9085aa6915be6d875
Chikara0216 <shaktiap1162002@gmail.com>
2023-05-20 16:24:44 +0530

    Added JWT_SECRET .env variable

--- server/src/middleware/verifyToken.js
+++ server/src/middleware/verifyToken.js
@@ -4,7 +4,7 @@ export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
-    jwt.verify(authHeader, 'secret', (err) => {
+    jwt.verify(authHeader, process.env.JWT_SECRET, (err) => {
       if (err) {
         return res.sendStatus(403);
       }
